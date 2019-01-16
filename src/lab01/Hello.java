2d7c00c4e24b6228da4302a39f64cf169bb401ce
Mitch Gavars <mag051@bucknell.edu>
2019-01-15 22:41:02 -0500

    Lab01, Exercise 5, Complete

---
+++ src/lab01/Hello.java
@@ -0,0 +1,25 @@
+/* *****************************************
+* CSCI205 - Software Engineering and Design
+* Spring 2019
+* Instructor: Prof. Dancy
+* Section: 1pm
+*
+* Name: Mitch Gavars
+* Date: 01/15/2019
+*
+* Lab / Assignment: Lab01 "Hello, CSCI 205!"
+*
+* Description: Begin learning how to use the Java programming language.
+*		A "Hello World" program in Java. 
+*
+* *****************************************/
+package lab01;
+
+public class Hello
+{
+	public static void main( String[] args )
+		{
+		System.out.println("Programming is not a spectator sport!");
+		System.exit(0);
+		}
+}
