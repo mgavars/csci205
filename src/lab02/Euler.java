326210e1c1fea2c3c7b4ba2a07e5e479a6a50a7f
Mitch Gavars <mag051@bucknell.edu>
2019-01-20 22:43:22 -0500

    Lab02, Exercise 4, complete

---
+++ src/lab02/Euler.java
@@ -0,0 +1,147 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Spring 2019
+ *
+ * Name: Mitch Gavars
+ * Date: 01/16/2019
+ *
+ * Lab: Lab02, Exercise 4
+ *
+ * Description:
+ * A simple exercise to to compute e, and to illustrate BigDecimal and
+ * its computational cost
+ * *****************************************
+ */
+package lab02;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
+/**
+ * Euler - a class of static methods to illustrate different ways to compute
+ * Euler's number
+ */
+public class Euler {
+
+    /**
+     * computeEulerTerm - compute the nth term of the power series of e^x
+     *
+     * @param n - The nth term of the power series to compute
+     * @param x - the power of e used
+     * @return - The computed term in the expansion
+     */
+    public static double computeEulerTerm(int n, double x) {
+        // TODO - Complete this method based on the code given in the lab
+	double numerator = 1.0, denominator = 1.0;
+
+	for (int i = 1; i <= n; i++) {
+		numerator *= x;
+		denominator *= i;
+		 
+	}
+	double term = numerator / denominator;
+        return term;
+    }
+
+    /**
+     * compute Euler's number to the x power using the Taylor series expansion.
+     * This is a bit inefficient. It calls a separate method to compute each
+     * term in the expansion.
+     *
+     * @param x - compute e^x
+     * @param numTerms - Number of terms to compute in the expansion
+     * @return Euler's number
+     */
+    public static double computeEulerSlow(double x, int numTerms) {
+        // TODO - Complete this method!
+	double slowEulerAnswer = 0.0;
+
+	for (int i = 1; i<= numTerms; i++) {
+		slowEulerAnswer += computeEulerTerm(i, x);
+	}
+        return 1.0 + slowEulerAnswer;
+    }
+
+    /**
+     * compute Euler's number to the x power using the Taylor series expansion.
+     * This is a faster implementation, using only one loop to compute the
+     * result.
+     *
+     * @param x - compute e^x
+     * @param numTerms - Number of terms to compute in the expansion
+     * @return Euler's number
+     */
+    public static double computeEulerFast(double x, int numTerms) {
+        // TODO - Complete this method!
+	double fastEulerAnswer = 0.0, numerator = 1.0, denominator = 1.0;
+
+	for (int i = 1; i<= numTerms; i++) {
+		numerator *= x;
+		denominator *= i;
+		fastEulerAnswer += numerator / denominator;
+	}
+	fastEulerAnswer += 1.0;
+        return fastEulerAnswer;
+    }
+
+    /**
+     * computeEulerBig - compute e using BigDecimal so that we do not lose
+     * accuracy
+     *
+     * @param x - the exponent of e to use
+     * @param numTerms - the number of terms of the series to compute
+     * @return - e as a BigDecimal
+     */
+    public static BigDecimal computeEulerBig(double x, int numTerms) {
+        // TODO - Complete this method!
+
+	// Define BigDecimal variables
+	BigDecimal numerator = new BigDecimal(1.0);
+	BigDecimal denominator = new BigDecimal(1.0);
+	BigDecimal term = new BigDecimal(1.0);
+	BigDecimal bigEulerAnswer = new BigDecimal(1.0);
+
+	for (int i = 1; i <= numTerms; i++) {
+
+		// Find new numerator and denominator	
+		numerator = numerator.multiply(new BigDecimal(x));
+		denominator = denominator.multiply(new BigDecimal(i));
+
+		// Get the new fraction and add it to the final answer
+		term = numerator.divide(denominator,20, RoundingMode.valueOf("HALF_UP"));
+		bigEulerAnswer = bigEulerAnswer.add(term);
+	}
+        return bigEulerAnswer;
+    }
+
+    /**
+     * Main program to test out Euler computations
+     */
+    public static void main(String args[]) {
+
+        final int X = 5;
+        final int NUM_TERMS = 20;
+
+        System.out.printf("Evaluating e^%d using %d terms:\n", X, NUM_TERMS);
+
+        long startTime = System.nanoTime();
+        double dAnswer = Math.exp(X);
+        long elapsedTime = System.nanoTime() - startTime;
+        System.out.printf("Math.exp answer:      %.20f : Elapsed time (ns): %d\n", dAnswer, elapsedTime);
+
+        startTime = System.nanoTime();
+        double num = computeEulerSlow(X, NUM_TERMS);
+        elapsedTime = System.nanoTime() - startTime;
+        System.out.printf("My slow answer:       %.20f : Elapsed time (ns): %d\n", num, elapsedTime);
+
+        startTime = System.nanoTime();
+        num = computeEulerFast(X, NUM_TERMS);
+        elapsedTime = System.nanoTime() - startTime;
+        System.out.printf("My fast answer:       %.20f : Elapsed time (ns): %d\n", num, elapsedTime);
+
+        startTime = System.nanoTime();
+        BigDecimal bigAnswer = computeEulerBig(X, NUM_TERMS);
+        elapsedTime = System.nanoTime() - startTime;
+        System.out.printf("My BigDecimal answer: %.20f : Elapsed time (ns): %d\n", bigAnswer, elapsedTime);
+    }
+}
