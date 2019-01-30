d6989ab79f00d212803e1dc6e39bb8a89cac8ccc
Mitch Gavars <mag051@bucknell.edu>
2019-01-30 13:35:00 -0500

    Lab05, Exercise 1, a simple hello world program

---
+++ src/lab05/Hello.java
@@ -0,0 +1,33 @@
+/* *****************************************
+* CSCI205 - Software Engineering and Design
+* Spring 2019
+*
+* Name: Mitch Gavars
+* Date: Jan 30, 2019
+* Time: 1:22:04 PM
+*
+* Project: csci205
+* Package: lab05
+* File: Hello
+* Description:
+*
+* ****************************************
+ */
+package lab05;
+
+/**
+ * A simple program to aid in understanding NetBeans
+ *
+ * @author Mitch Gavars
+ * @version 0.1
+ */
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Becoming a good programmer takes practice");
+    }
+
+}
