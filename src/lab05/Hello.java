6274e88bfeb182389703ffdbec27f8367a6d1c38
Mitch Gavars <mag051@bucknell.edu>
2019-01-31 18:49:45 -0500

    Lab05, Exercise 1, Complete

--- src/lab05/Hello.java
+++ src/lab05/Hello.java
@@ -15,6 +15,10 @@
  */
 package lab05;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * A simple program to aid in understanding NetBeans
  *
@@ -23,11 +27,24 @@ package lab05;
  */
 public class Hello {
 
+    private static final int NUM_INTS = 10;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Becoming a good programmer takes practice");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Hello. What is your name? ");
+        String name = in.next();
+        System.out.println(name + ", becoming a good programmer takes practice");
+
+        Random rand = new Random();
+        int[] x = new int[NUM_INTS];
+        for (int i = 0; i < NUM_INTS; i++) {
+            x[i] = rand.nextInt(100);
+        }
+
+        System.out.println(Arrays.toString(x));
     }
 
 }
