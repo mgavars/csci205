f3b52ee556cc0ce3fb08316c9eda84a5b7aa7597
Mitch Gavars <mag051@bucknell.edu>
2019-02-08 13:18:48 -0500

    Lab08, Exercise 1, complete

---
+++ src/lab08/Coin.java
@@ -0,0 +1,25 @@
+/* *****************************************
+* CSCI205 - Software Engineering and Design
+* Spring 2019
+*
+* Name: Mitch Gavars
+* Date: Feb 6, 2019
+* Time: 1:36:46 PM
+*
+* Project: csci205
+* Package: lab08
+* File: Coin
+* Description:
+*
+* ****************************************
+ */
+package lab08;
+
+/**
+ * Coin - a simple enumerated type to represent the different types of coins
+ *
+ * @author mag051
+ */
+public enum Coin {
+    NICKEL, DIME, QUARTER;
+}
