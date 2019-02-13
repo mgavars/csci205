68b4f62a6578f7d27becb51e0275667474e4e6bb
Mitch Gavars <mag051@bucknell.edu>
2019-02-13 17:50:37 -0500

    Lab09, Exercise 4, complete

--- test/lab09/CashRegisterTest.java
+++ test/lab09/CashRegisterTest.java
@@ -42,6 +42,7 @@ public class CashRegisterTest extends TestCase {
     @Override
     protected void tearDown() throws Exception {
         super.tearDown();
+        instance = null;
     }
 
     /**
