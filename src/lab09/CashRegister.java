46110aa81960174e3a24bfc13e1f745813a92853
Mitch Gavars <mag051@bucknell.edu>
2019-02-13 17:44:18 -0500

    Lab09, Exercise 3, complete

--- src/lab09/CashRegister.java
+++ src/lab09/CashRegister.java
@@ -143,29 +143,23 @@ public class CashRegister {
         paymentCollected = 0;
         return change;
     }
-
-    public static void main(String[] args) {
-        CashRegister myRegister = new CashRegister();
-        myRegister.scanItem(1.55);
-        myRegister.scanItem(2.27);
-        System.out.println("Purchases: " + myRegister.getListOfPurchases());
-        System.out.println("Expected: [1.55, 2.27]");
-        myRegister.collectPayment(Money.DOLLAR, 3);
-        myRegister.collectPayment(Money.QUARTER, 3);
-        myRegister.collectPayment(Money.NICKEL, 2);
-        System.out.println("Payment made: " + myRegister.getPaymentCollected());
-        System.out.println("Expected: 3.85");
-
-        try {
-            double myChange = myRegister.giveChange();
-            System.out.println("Change: " + myChange);
-            System.out.println("Expected: 0.03");
-        } catch (ChangeException changeException) {
-            System.err.println(changeException.getMessage());
-        }
-
-        //Check for an invalid price
-        //myRegister.scanItem(-0.50);
-    }
+    /**
+     * public static void main(String[] args) { CashRegister myRegister = new
+     * CashRegister(); myRegister.scanItem(1.55); myRegister.scanItem(2.27);
+     * System.out.println("Purchases: " + myRegister.getListOfPurchases());
+     * System.out.println("Expected: [1.55, 2.27]");
+     * myRegister.collectPayment(Money.DOLLAR, 3);
+     * myRegister.collectPayment(Money.QUARTER, 3);
+     * myRegister.collectPayment(Money.NICKEL, 2); System.out.println("Payment
+     * made: " + myRegister.getPaymentCollected());
+     * System.out.println("Expected: 3.85");
+     *
+     * try { double myChange = myRegister.giveChange();
+     * System.out.println("Change: " + myChange); System.out.println("Expected:
+     * 0.03"); } catch (ChangeException changeException) {
+     * System.err.println(changeException.getMessage()); }
+     *
+     * //Check for an invalid price //myRegister.scanItem(-0.50); }
+     */
 
 }
---
+++ test/lab09/CashRegisterTest.java
@@ -0,0 +1,183 @@
+/* *****************************************
+* CSCI205 - Software Engineering and Design
+* Spring 2019
+*
+* Name: Mitch Gavars
+* Date: Feb 11, 2019
+* Time: 1:50:00 PM
+*
+* Project: csci205
+* Package: lab09
+* File: CashRegisterTest
+* Description:
+*
+* ****************************************
+ */
+package lab09;
+
+import java.util.LinkedList;
+import java.util.List;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author mag051
+ */
+public class CashRegisterTest extends TestCase {
+
+    static final double EPSILON = 1.0E-12;
+
+    public CashRegisterTest(String testName) {
+        super(testName);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test of getPurchaseCount method, of class CashRegister.
+     */
+    public void testGetPurchaseCount() {
+        System.out.println("getPurchaseCount");
+        CashRegister instance = new CashRegister();
+
+        // Test the initial state. It should have no items
+        assertEquals(0, instance.getPurchaseCount());
+
+        // Now, set up a test of two items
+        instance.scanItem(.55);
+        instance.scanItem(1.27);
+        int expResult = 2;
+        int result = instance.getPurchaseCount();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getListOfPurchases method, of class CashRegister.
+     */
+    public void testGetListOfPurchases() {
+        System.out.println("getListOfPurchases");
+        CashRegister instance = new CashRegister();
+
+        instance.scanItem(.55);
+        instance.scanItem(1.27);
+        List<Double> expResult = new LinkedList<Double>();
+        expResult.add(.55);
+        expResult.add(1.27);
+        List<Double> result = instance.getListOfPurchases();
+        assertEquals(expResult, result);
+
+    }
+
+    /**
+     * Test of getTransactionTotal method, of class CashRegister.
+     */
+    public void testGetTransactionTotal() {
+        System.out.println("getTransactionTotal");
+        CashRegister instance = new CashRegister();
+        assertEquals(0, instance.getTransactionTotal(), EPSILON);
+
+        // Now, set up a test of two items
+        instance.scanItem(1.27);
+        instance.scanItem(.55);
+        double expResult = 1.82;
+        double result = instance.getTransactionTotal();
+        assertEquals(expResult, result, EPSILON);
+
+    }
+
+    /**
+     * Test of getPaymentCollected method, of class CashRegister.
+     */
+    public void testGetPaymentCollected() {
+        System.out.println("getPaymentCollected");
+        CashRegister instance = new CashRegister();
+        assertEquals(0, instance.getPaymentCollected(), EPSILON);
+
+        instance.collectPayment(Money.NICKEL, 5);
+        instance.collectPayment(Money.DIME, 1);
+        instance.collectPayment(Money.TWENTY, 2);
+        double expResult = 40.35;
+        double result = instance.getPaymentCollected();
+        assertEquals(expResult, result, EPSILON);
+
+    }
+
+    /**
+     * Test of collectPayment method, of class CashRegister.
+     */
+    public void testCollectPaymentException() {
+        System.out.println("collectPayment");
+        Money moneyType = null;
+        int unitCount = 0;
+        CashRegister instance = new CashRegister();
+
+        try {
+            instance.collectPayment(moneyType, -1);
+            fail("Improper checking for negative count in collectPayment");
+        } catch (IllegalArgumentException expected) {
+        }
+    }
+
+    /**
+     * Test of giveChange method, of class CashRegister.
+     */
+    public void testGiveChange() throws Exception {
+        System.out.println("giveChange");
+        CashRegister instance = new CashRegister();
+        assertEquals(0, instance.getPaymentCollected(), EPSILON);
+
+        instance.scanItem(1.27);
+        instance.scanItem(.55);
+        instance.collectPayment(Money.TWENTY, 1);
+        double expResult = 18.18;
+        double result = instance.giveChange();
+        assertEquals(expResult, result, EPSILON);
+    }
+
+    /**
+     * Test of giveChange method, of class CashRegister.
+     */
+    public void testGiveChangeException() throws Exception {
+        System.out.println("giveChange");
+        CashRegister instance = new CashRegister();
+        assertEquals(0, instance.getPaymentCollected(), EPSILON);
+
+        instance.scanItem(1.27);
+        instance.scanItem(.55);
+        instance.scanItem(20.0);
+        instance.collectPayment(Money.TWENTY, 1);
+        try {
+            instance.giveChange();
+            fail("Improper checking for giveChange()");
+        } catch (ChangeException expected) {
+        }
+    }
+
+    /**
+     * Test for a scanned item to be bad and thrown an IllegalArgumentExpression
+     */
+    public void testScanItemIllegalArgumentException() {
+        CashRegister instance = new CashRegister();
+        boolean wasThrown = false;
+        try {
+            instance.scanItem(-.5);
+            fail("Improper checking for low scanItem()");
+        } catch (IllegalArgumentException expected) {
+        }
+        try {
+            // Now, let's scan for a large value
+            instance.scanItem(1.0E6);
+            fail("Improper checking for high scanItem()");
+        } catch (IllegalArgumentException expected) {
+        }
+    }
+
+}
